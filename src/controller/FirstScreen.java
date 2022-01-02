43813765619fee9d3b626025d32633be7ccab63f
Bart Lyons <lyons.bart@protonmail.com>
2022-01-02 13:05:30 -0500

    Finished video with test button and text.

--- src/controller/FirstScreen.java
+++ src/controller/FirstScreen.java
@@ -1,13 +1,22 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FirstScreen implements Initializable {
+    public Label theLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("I am initialized! ");
+        theLabel.setText("Initialized");
+    }
+
+    public void onButtonAction(ActionEvent actionEvent) {
+        System.out.println("I am clicked");
+        theLabel.setText("You clicked it!");
     }
 }
