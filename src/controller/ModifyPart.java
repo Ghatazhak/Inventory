ba93f42a88d7cc27053fb033ca168eca6259a78e
Bart Lyons <lyons.bart@protonmail.com>
2022-01-03 12:14:47 -0500

    started implementation of parts classes.

--- src/controller/ModifyPart.java
+++ src/controller/ModifyPart.java
@@ -5,11 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ModifyPart {
+    public RadioButton inHouse;
+    public RadioButton outSourced;
+    public Label inHouseMachineIDLabel;
+    public Label outSourcedCompanyNameLabel;
+    public TextField outSourcedCompanyNameTextField;
+    public TextField inHouseMachineIDTextField;
+
     public void saveModifyPart(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
         Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
@@ -28,5 +38,19 @@ public class ModifyPart {
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void inHouse(ActionEvent actionEvent) {
+        inHouseMachineIDLabel.setVisible(true);
+        inHouseMachineIDTextField.setVisible(true);
+        outSourcedCompanyNameLabel.setVisible(false);
+        outSourcedCompanyNameTextField.setVisible(false);
+    }
+
+    public void outSourced(ActionEvent actionEvent) {
+        outSourcedCompanyNameLabel.setVisible(true);
+        outSourcedCompanyNameTextField.setVisible(true);
+        inHouseMachineIDLabel.setVisible(false);
+        inHouseMachineIDTextField.setVisible(false);
     }
 }
